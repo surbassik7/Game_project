1241d4fec3d74dffc31cbd61defdbb9b8b36b9a8
Sean Urbassik <sean.urbassik@gmail.COM>
2019-05-21 16:06:56 -0400

    added questions

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+const myQuestions = [
+  {
+    question: "What alloy is Captain America's sheild made of?",
+    answers: {
+      a: "Steel",
+      b: "Adamantium",
+      c: "Vibrainium",
+      d: "Uru"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question:
+      "What was the code name of the Government project that gave Captain America his powers?",
+    answers: {
+      a: "Weapon X",
+      b: "Super Soldier",
+      c: "AIM",
+      d: "Hyrda"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question:
+      "What was the name of the Virtual Intellegnce designed by Iron man?",
+    answers: {
+      a: "Jarvis",
+      b: "Hal 9000",
+      c: "T-800",
+      d: "R2-D2"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question:
+      "What did Iron man build to power his suits and keep himself alive?",
+    answers: {
+      a: "Skynet",
+      b: "Death Star",
+      c: "Gamma Bomb",
+      d: "Arc Reactor"
+    },
+    correctAnswer: "d"
+  }
+];
